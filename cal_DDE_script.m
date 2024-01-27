bddbdadba2bf6ff175f592e9664a7b819fcf39cb
Zixin Zhang <1186193932@qq.com>
2024-01-26 19:37:52 -0600

    Update data sampling method
    More dense sampling and observables in mode 2

--- cal_DDE_script.m
+++ cal_DDE_script.m
@@ -1,8 +1,11 @@
+addpath("./functions")
+
 clear;
-cal_DDE(750, 2.5);
-clear;
-cal_DDE(750, 3);
-clear;
-cal_DDE(750, 2.5);
-clear;
+cal_DDE(300, 50, 2, 18.5);
+
+!git pull
+!git add *
+!git commit -m "DDE result from the server"
+!git push
+
 quit;
--- functions/cal_DDE.m
+++ functions/cal_DDE.m
@@ -1,46 +1,66 @@
-function cal_DDE(n_obs, eps)
+function cal_DDE(n_obs_1, n_obs_2, eps_1, eps_2)
+% n_obs_1 = 300, n_obs_2 = 50, eps_1 = 2, eps_2 = 18.5 may be a good choice
 addpath("./functions")
-
 syms h v u real
 x = [h; v];
 
+n_obs = n_obs_1 + n_obs_2;
+g = -9.81;
 h_min = 0; % must be zero
-h_max = 5;
-v_min = -2.5;
-v_max = 2.5;
-[H, V] = meshgrid(h_min:0.1:h_max, v_min:0.1:v_max);
-state_points = [H(:), V(:)];
+h_max = 1;
+v0_max = 1;
+v_max = sqrt(-2*g*h_max+v0_max^2);
+v_min = -v_max;
+v_jump = -4.3;
+h_jump = 0.05;
 
+% sample
+[H1, V1] = meshgrid(h_min:0.05:h_jump, v_jump:0.05:v_max);
+[H2, V2] = meshgrid(h_min:0.01:h_jump, v_min:0.01:v_jump); % approximated mode 2 reagion
+[H3, V3] = meshgrid(h_jump:0.05:h_max, v_min:0.05:v_max+0.1);
+
+state_points_1 = [[H1(:), V1(:)]; [H3(:), V3(:)]]; % mode 1
+state_points_2 = [H2(:), V2(:)]; % mode 2, contact
+state_points = unique([state_points_1; state_points_2], 'rows');
 state_next_points = zeros(size(state_points));
 for i = 1:size(state_points, 1)
     state_next = bouncing_ball_dynamics(state_points(i, :)', 0);
     state_next_points(i, :) = state_next';
 end
-
 state_DT = delaunayTriangulation(state_points);
-state_IC = incenter(state_DT);
-dv = 0.5 * 0.1 * 0.1;
+
+% figure(1);
+% triplot(state_DT), axis equal, grid on;
 
 % RBF observables
-[~, rbf_center_list] = kmeans(state_points, n_obs);
+[~, rbf_center_list_1] = kmeans(unique(state_points_1, 'rows'), n_obs_1); % mode 1
+[~, rbf_center_list_2] = kmeans(unique(state_points_2, 'rows'), n_obs_2); % mode 2
+rbf_center_list = [rbf_center_list_1; rbf_center_list_2];
+
 g_list = sym(zeros(n_obs, 1));
-for k = 1:n_obs
+for k = 1:n_obs_1
     rbf_center = rbf_center_list(k, :);
-    g_list(k) = exp(-eps^2*(x - rbf_center')'*(x - rbf_center'));
+    g_list(k) = exp(-eps_1^2*(x - rbf_center')'*(x - rbf_center'));
 end
+for k = n_obs_1 + 1:n_obs
+    rbf_center = rbf_center_list(k, :);
+    g_list(k) = exp(-eps_2^2*(x - rbf_center')'*(x - rbf_center'));
+end
+
+% figure(2);
+% plot(rbf_center_list(:, 1), rbf_center_list(:, 2), 'o'), axis equal, grid on;
+
 g_list = [x; g_list]; % add state into observables
 n_obs = n_obs + 2;
 
 % estimate R
 R = zeros(n_obs, n_obs);
-f_fun = matlabFunction(x+u, 'Vars', {x, u});
 for i = 1:n_obs
     parfor j = i:n_obs
         fprintf('Estimate R matrix: i = %d, j = %d\n', i, j);
         gi_fun = matlabFunction(g_list(i), 'Vars', {x});
         gj_fun = matlabFunction(g_list(j), 'Vars', {x});
-        result = estimate_inner_product(gi_fun, gj_fun, f_fun, ...
-                                        state_DT, dv);
+        result = estimate_inner_product(gi_fun, gj_fun, state_points, state_DT);
         R(i, j) = result;
     end
 end
@@ -53,8 +73,7 @@ for i = 1:n_obs
         fprintf('Estimate Q matrix: i = %d, j = %d\n', i, j);
         gi_fun = matlabFunction(g_list(i), 'Vars', {x});
         gj_fun = matlabFunction(g_list(j), 'Vars', {x});
-        result = estimate_inner_product(gi_fun, gj_fun, @bouncing_ball_dynamics, ...
-                                        state_DT, dv);
+        result = estimate_inner_product(gi_fun, gj_fun, state_next_points, state_DT);
         Q(i, j) = result;
     end
 end
@@ -68,7 +87,7 @@ under_line_arr = repmat("_", size(time_arr));
 time_arr = [time_arr; under_line_arr];
 time_arr = time_arr(:)';
 time_arr = time_arr(1:end-2);
-file_name = strjoin(time_arr, '') + "bouncing_ball_2_dim_DDE.mat";
-save(file_name, "R", "Q", "A", "rbf_center_list", "eps", "g_list");
+file_name = "./data/" + strjoin(time_arr, '') + "bouncing_ball_2_dim_DDE.mat";
+save(file_name, "R", "Q", "A", "rbf_center_list", "n_obs_1", "n_obs_2", "eps_1", "eps_2", "g_list");
 
 end
