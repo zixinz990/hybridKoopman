7408bb2e722dd4539d6cf766c17de14f5c5ecac1
Zixin Zhang <1186193932@qq.com>
2024-03-04 18:40:31 -0600

    Remove pre run process

--- functions/gen_data.m
+++ functions/gen_data.m
@@ -1,4 +1,4 @@
-function [data, data_next] = gen_data(dyn_fun, sample_method, options)
+function [data, data_extended, data_next] = gen_data(dyn_fun, sample_method, options)
 % Generate data for KDE
 % Input:
 %   dyn_fun:       function handle of dynamics
@@ -13,6 +13,8 @@ arguments
     options.traj_step_size(2, 1) {mustBeNumeric, mustBePositive} = [0.05; 0.05]
     options.var_density_min_step_size(2, 1) {mustBeNumeric, mustBePositive} = [0.005; 0.005]
     options.var_density_max_step_size(2, 1) {mustBeNumeric, mustBePositive} = [0.02; 0.02]
+    options.u_min(1, 1) {mustBeNumeric} = -10;
+    options.u_max(1, 1) {mustBeNumeric}  = 10;
 end
 
 g = 10;
@@ -25,12 +27,13 @@ if sample_method == "uniform"
     v_max = sqrt(2*g*h0_max+v0_max^2);
     v_min = -v_max;
     h_max = h0_max + 0.5 * v0_max^2 / g;
+    h_min = h_min - 0.15; % extend the lower bound a bit, so that more observables can cover the h=0 bound
     x_range = [h_min, h_max; ...
                v_min, v_max];
 
     % Sample
     step_size = options.uniform_step_size;
-    data = uniform_sampling_2_dim(x_range, step_size);
+    data_extended = uniform_sampling_2_dim(x_range, step_size);
 elseif sample_method == "trajectory"
     % Setup dynamics
     x_init_range = [0, 0.5; ...
@@ -41,11 +44,12 @@ elseif sample_method == "trajectory"
     % Sample
     num_steps = 100;
     step_size = options.traj_step_size;
-    data = traj_sampling_2_dim(x_init_range, u_range, dyn_fun, num_steps, step_size = step_size);
-    data = cell2mat(reshape(data, size(data, 2), 1));
+    data_extended = traj_sampling_2_dim(x_init_range, u_range, dyn_fun, num_steps, step_size = step_size);
+    data_extended = cell2mat(reshape(data_extended, size(data_extended, 2), 1));
 elseif sample_method == "var_density"
     % Setup dynamical range
     h_min = 0; % must be zero
+    h_min = h_min - 0.15; % extend the lower bound a bit, so that more observables can cover the h=0 bound
     h0_max = 0.5;
     v0_max = 0.5;
     v_max = sqrt(2*g*h0_max+v0_max^2);
@@ -57,17 +61,23 @@ elseif sample_method == "var_density"
     % Sample
     min_step_size = options.var_density_min_step_size;
     max_step_size = options.var_density_max_step_size;
-    data = var_density_sampling_bouncing_ball_2_dim(x_range, min_step_size, max_step_size);
+    data_extended = var_density_sampling_bouncing_ball_2_dim(x_range, min_step_size, max_step_size);
 end
 
 % Remove all data that h = 0 and v <= 0
-bad_x_init_idx = data(:, 1) == 0 & data(:, 2) <= 0;
-data(bad_x_init_idx, :) = [];
+% bad_x_init_idx = data_extended(:, 1) == 0 & data_extended(:, 2) <= 0;
+% data_extended(bad_x_init_idx, :) = [];
+% bad_x_init_idx_2 = data_extended(:, 1) < 0;
+infeas_x_idx = data_extended(:, 1) <= 0;
+tmp = data_extended;
+data_extended(infeas_x_idx, :) = [];
+data = data_extended;
+data_extended = tmp;
 
 % Generate list of control input
 u_list = zeros(size(data, 1), 1);
 for i = 1:size(u_list, 1)
-    u_list(i, :) = 30 * rand - 10;
+    u_list(i, :) = rand * (options.u_max - options.u_min) + options.u_min;
 end
 data = [data, u_list];
 
--- run.m
+++ run.m
@@ -1,49 +1,103 @@
-function run(filename)
-addpath("./functions");
-addpath("./data");
-load(filename);
-obs_fun_list = obs_fun_list;
-data = data;
-data_next = data_next;
-data_DT = data_DT;
-rbf_center_list = rbf_center_list;
-num_rbf = num_rbf;
-eps = eps;
-obs_fun_list = obs_fun_list;
+function run(num_rbf, eps, u_min, u_max)
+%% Sample data
+dyn_fun = @bouncing_ball_2_dim_dyn;
+sample_method = "uniform";
+uniform_step_size = [0.015; 0.015];
+% In the data matrix, each row is [x1, x2, u]
+[data, data_extended, data_next] = gen_data(dyn_fun, sample_method, ...
+                                            uniform_step_size=uniform_step_size, ...
+                                            u_min=u_min, u_max=u_max);
+fprintf("Number of data: %d\n", size(data, 1));
 
-% Estimate R matrix
-R = zeros(num_obs, num_obs);
-for i = 1:num_obs
-    parfor j = i:num_obs
+%% Delaunay triangulation
+data_DT = delaunayTriangulation(data(:, 1:2));
+
+%% Generate RBFs
+% Use extended dataset to cover the h=0 boundry better
+[rbf_fun_cell, rbf_centers] = gen_gaussian_rbfs_2_dim(num_rbf, eps, data_extended(:, 1:2));
+
+%% Generate observable function cell
+obs_fun_cell = rbf_fun_cell;
+syms h v u real
+x = [h; v];
+obs_fun_cell{end+1} = matlabFunction(h, 'Vars', {x});
+obs_fun_cell{end+1} = matlabFunction(v, 'Vars', {x});
+obs_fun_cell{end+1} = matlabFunction(u, 'Vars', {[x; u]});
+obs_dim = size(obs_fun_cell, 2);
+
+%% Estimate R matrix
+R = zeros(obs_dim, obs_dim);
+for i = 1:obs_dim
+    parfor j = i:obs_dim
         fprintf('Estimating R matrix: i = %d, j = %d\n', i, j);
-        result = est_inner_product(obs_fun_list{i}, obs_fun_list{j}, data, data_DT);
+        result = est_inner_product(obs_fun_cell{i}, obs_fun_cell{j}, data, data_DT);
         R(i, j) = result;
     end
 end
 R = R + R' - tril(R);
 
-% Estimate Q matrix
-Q = zeros(num_obs, num_obs);
-for i = 1:num_obs
-    parfor j = 1:num_obs
+%% Estimate Q matrix
+Q = zeros(obs_dim, obs_dim);
+for i = 1:obs_dim
+    parfor j = 1:obs_dim
         if j == 5
             disp("")
         end
         fprintf('Estimating Q matrix: i = %d, j = %d\n', i, j);
-        result = est_inner_product(obs_fun_list{i}, obs_fun_list{j}, data_next, data_DT);
+        result = est_inner_product(obs_fun_cell{i}, obs_fun_cell{j}, data_next, data_DT);
         Q(i, j) = result;
     end
 end
 
-% Get A matrix
+%% Get A matrix
 A = Q * inv(R);
 
-% save
+%% Save
 time_arr = string(clock);
 under_line_arr = repmat("_", size(time_arr));
 time_arr = [time_arr; under_line_arr];
 time_arr = time_arr(:)';
 time_arr = time_arr(1:end-2);
 file_name = "./data/" + strjoin(time_arr, '') + "bouncing_ball_2_dim_KDE.mat";
-save(file_name, "R", "Q", "A", "rbf_center_list", "num_rbf", "eps", "obs_fun_list");
+save(file_name, "R", "Q", "A", ...
+                "num_rbf", "eps", "u_min", "u_max", "sample_method", "uniform_step_size", ...
+                "rbf_centers", "obs_dim", "obs_fun_cell");
+
+%% Plot
+% figure(1);
+% 
+% subplot(1, 4, 1);
+% plot(data(:, 1), data(:, 2), '.');
+% xlabel("Height (m)", 'FontSize', 16);
+% ylabel("Velocity (m/s)", "FontSize", 16);
+% ylim([-4, 4]);
+% title("Uniform Sampling", "FontSize", 24);
+% axis equal, grid on;
+% 
+% subplot(1, 4, 2);
+% plot(data_next(:, 1), data_next(:, 2), '.');
+% xlabel("Height (m)", 'FontSize', 16);
+% ylabel("Velocity (m/s)", "FontSize", 16);
+% title("Uniform Sampling (next)", "FontSize", 24);
+% ylim([-4, 4]);
+% axis equal, grid on;
+% 
+% subplot(1, 4, 3);
+% triplot(data_DT);
+% ylim([-4, 4]);
+% axis equal, grid on;
+% 
+% subplot(1, 4, 4);
+% rbf_08_radius = sqrt(-log(0.2) / eps^2); % radius of 20%
+% hold on;
+% for i = 1:num_rbf
+%     ci = rbf_centers(i, :);
+%     draw_circle(ci(1), ci(2), rbf_08_radius);
+% end
+% xlabel("Height (m)", 'FontSize', 16);
+% ylabel("Velocity (m/s)", "FontSize", 16);
+% title("Center of Observables with 20% radius", "FontSize", 24);
+% ylim([-4, 4]);
+% axis equal; grid on;
+
 end
